25d7979dc93a1a3c6c429a7f30f10e80cc416728
Quang D. <ngocquang.nq98@gmail.com>
2025-12-31 10:26:32 +0700

    update gta

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -7,11 +7,12 @@ declare global {
 
 export const gtmEvent = (eventName: string, params?: Record<string, any>) => {
   if (typeof window !== 'undefined' && window.dataLayer) {
+    console.log("GTM dataLayer found");
     window.dataLayer.push({
       event: eventName,
       ...params,
     });
   } else {
-    // console.warn("GTM dataLayer not found");
+    console.warn("GTM dataLayer not found");
   }
 };
